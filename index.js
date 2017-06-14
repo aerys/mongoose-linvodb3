ec467e116a9ebba1c7c9d81ff028c23aaad759f2
Julien Cloute <julien.cloute@aerys.in>
2017-06-14 14:12:44 +0200

    Handle find functions called without callback.

--- index.js
+++ index.js
@@ -103,7 +103,8 @@ module.exports = {
                                 {
                                     model.emit('find', result);
 
-                                    return callback(error, result);
+                                    if (!!callback)
+                                        callback(error, result);
                                 }
                             ]);
                         };
